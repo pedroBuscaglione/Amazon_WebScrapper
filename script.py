b632fd75e61663b59e5ba6b3b3f2934b8051144e
Pedro Buscaglione <psbuscaglione@gmail.com>
2024-10-30 22:36:57 -0300

    Update script.py

--- script.py
+++ script.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
-import time
+
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
 
 # Defina o caminho do ChromeDriver
-service = Service("C:\chromedriver.exe") 
+service = Service("C:/chromedriver.exe") 
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")  # Executa o Chrome em modo headless (sem abrir janela)
 options.add_argument("--disable-gpu")
@@ -18,23 +22,32 @@ driver = webdriver.Chrome(service=service, options=options)
 url = "https://www.amazon.com.br/Livros/"
 driver.get(url)
 
-# Aguarde alguns segundos para garantir que o JavaScript carregue todo o conteúdo
-time.sleep(5)  # Ajuste o tempo de espera, se necessário
+# Aguarda até que os elementos carreguem
+try:
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, 'a-truncate-full'))
+    )
+except Exception as e:
+    print("Erro ao carregar a página:", e)
 
 # Extrai o HTML da página renderizada
 html = driver.page_source
 
-# Salve o HTML em um arquivo para análise
-with open("selenium_response.html", "w", encoding="utf-8") as file:
-    file.write(html)
-
-# Use BeautifulSoup para analisar o HTML
+# Uso do BeautifulSoup para analisar o HTML
 soup = BeautifulSoup(html, 'html.parser')
 
-# Exemplo de extração: ajuste o seletor conforme a estrutura da página
-books = soup.find_all('span', class_='a-truncate-cut')  # Verifique se a classe está correta
+in_offer_books = False # Boolean para separar os livros em discouto dos livros mais vendidos
+
+# Extração dos títulos dos livros
+books = soup.find_all('span', class_='a-truncate-full a-offscreen')
 for book in books:
-    print(book.get_text(strip=True))  # Exibe o título do livro
+    if book.text == "Livros em Oferta":
+        in_offer_section = True
+        continue  # Ignora essa linha
+    
+    # Se entrou na seção de ofertas seleciona esse livro
+    if in_offer_section: 
+        print(book.get_text(strip=True).replace('\u200f', '').replace('\u200e', ''))
 
 # Encerra o driver do Chrome
 driver.quit()
