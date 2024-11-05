963f9d4e8d1b85d87022ea1a09c2fc6c5960726e
Pedro Buscaglione <psbuscaglione@gmail.com>
2024-11-05 19:04:24 -0300

    Update script.py

--- script.py
+++ script.py
@@ -5,7 +5,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
-import re
+import os
+from datetime import datetime
+import pandas as pd
 import smtplib
 import schedule
 import time
@@ -68,6 +70,21 @@ def scrape_and_send_email():
 
         except IndexError:
             print("Elemento faltando, verifique a estrutura da página.")
+    
+    # Cria um DataFrame com os dados dos livros
+    df = pd.DataFrame(books, columns=['Título', 'Preço com Desconto', 'Preço Original'])
+
+    current_date = datetime.now().strftime("%d-%m-%Y") # Data atual no formato dia-mês-ano
+    file_name = f"Livros em Oferta - {current_date}.csv" # Nome do arquivo que vai ser criado
+
+    # Caminho da pasta onde o arquivo será criado
+    folder_path = "C:/Users/Usuario/OneDrive/Web Development/Projects/Amazon_WebScrapper/tabelas"
+    
+    # Cria o caminho do arquivo
+    file_path = os.path.join(folder_path, file_name)
+
+    # Salva o DataFrame em um CSV
+    df.to_csv(file_name, index=False, encoding='utf-8') 
 
     # Lê o arquivo com dados sensíveis
     try:
@@ -100,7 +117,7 @@ def scrape_and_send_email():
         print("Erro ao enviar o e-mail:", e)
 
 # Horário para a execução do scraping e envio do e-mail uma vez por dia
-schedule.every().day.at("15:37").do(scrape_and_send_email)
+schedule.every().day.at("18:56").do(scrape_and_send_email)
 
 # Loop que mantém o script rodando
 while True:
